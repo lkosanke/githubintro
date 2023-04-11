fcd9b986bd10f9cd9a1f53d58141c3fc8e109769
leoniehagitte <108812346+LeonieHagitte@users.noreply.github.com>
2023-04-11 13:28:06 +0200

    Update models_in _steno.jl

--- models_in _steno.jl
+++ models_in _steno.jl
@@ -1,16 +1,5 @@
 import Pkg; Pkg.add("StenoGraphs")
 using StenoGraphs #to select the package in julia
-#Model 1
-C → A
-[A C] → B
-#Model 2
-A → C
-C → D
-#Model 3
-B → C
-[B C] → D
-#Combined Model
-A → B
-C ⇒ A
-C ⇒ B
-[B C] → D
+@StenoGraph 
+Whole Model in one line
+A → B → D ← C ⇔ [A B]
