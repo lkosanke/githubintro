879c5e7c47baf0b9f2fe7a0be2120a48e2f5ebd6
lkosanke <kosankle@student.hu-berlin.de>
2023-04-19 10:56:48 +0200

    closes #16

--- model1 & model1 + model3.R
+++ model1 & model1 + model3.R
@@ -1,11 +1,23 @@
-#Model 1 seperately
 library(lavaan)
 
+#Model 1 seperately
 model1 <-'
 A ~ C
 B ~ A + C
 '
 
-#combination model 1 & 3
+#model 3 seperately
+model3 <-'
 B ~ C
 D ~ C + B
+'
+
+#combination model 1 & 3
+
+model1_3 <-'
+A ~ C
+B ~ A + C
+B ~ C
+D ~ C + B
+
+
